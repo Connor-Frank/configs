0a7eb64b8b6b0c8855ca93c6813875c2392f19de
Connor Frank <connorjfrank@gmail.com>
2021-02-26 09:24:53 -0500

    updates

---
+++ config/fish/config.fish
@@ -0,0 +1,71 @@
+# default editor
+set -Ux EDITOR vim
+
+# for pyenv
+#set PYENV_ROOT $HOME/.pyenv
+#set -x PATH $PYENV_ROOT/shims $PYENV_ROOT/bin $PATH
+#pyenv rehash
+
+# x86 homebrew
+alias xbrew='arch -x86_64 /usr/local/bin/brew'
+funcsave xbrew
+
+# this is an interesting package, if you mess up a command you can type "fuck" in terminal and it guesses at what you meant
+thefuck --alias | source
+
+function fcode
+	~/sync/scripts/./fcode.sh
+end
+funcsave fcode
+
+# creates a tmux session named as the current directory and containing two windows
+function tmx
+  set name (basename $PWD)
+  tmux new-session -d -s $name
+  tmux new-window -d
+  tmux attach-session -d -t $name
+end
+funcsave tmx
+
+# you can find this script in my repository "scripts"
+alias build-cmake='~/projects/scripts/cmake/build.sh'
+funcsave build-cmake
+
+# you can find this script in my repository "scripts"
+function run-cmake
+  set runfile (~/projects/scripts/cmake/run.sh)
+  $runfile $argv[1..-1]
+end
+funcsave run-cmake
+
+# vscode insiders runs on M1 but i want to use it the same way as the traditional one
+alias code='code-insiders'
+funcsave code
+
+# launch vscode insiders at current directory and close terminal
+alias vs='code-insiders . && exit'
+funcsave vs
+
+# exa is a cooler ls command that is colored and has file/folder icons
+alias show='exa -al --icons'
+funcsave show
+
+# my favorite way to run tty-clock
+alias clock='tty-clock -s -c -f "%d.%m.%Y"'
+funcsave clock
+
+# a cool clock i found (https://github.com/joshdk/tty-qlock)
+alias textclock='~/sync/scripts/tty-qlock-1.0.0/./dist/qlock -on-color green'
+funcsave textclock
+
+# this is the way i always run this package, so i created an alias
+alias mac_info='osx-cpu-temp -c -g -f -F'
+funcsave mac_info
+
+# gets rid of the fish greeting
+set fish_greeting
+
+# for iterm
+test -e /Users/connorfrank/.iterm2_shell_integration.fish ; and source /Users/connorfrank/.iterm2_shell_integration.fish ; or true
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+set -g fish_user_paths "/opt/homebrew/opt/qt/bin" $fish_user_paths
---
+++ vim/plugin-config/master-install.vim
@@ -0,0 +1,40 @@
+call plug#begin('~/.vim/pack')
+
+" themes
+Plug 'doums/darcula'
+Plug 'morhetz/gruvbox'
+Plug 'arcticicestudio/nord-vim'
+
+" lightline
+Plug 'itchyny/lightline.vim'
+Plug 'itchyny/vim-gitbranch'
+
+" autocompletion of pairs
+Plug 'jiangmiao/auto-pairs'
+" auto commenter
+Plug 'preservim/nerdcommenter'
+
+" tagbar (on the right, shows methods)
+Plug 'preservim/tagbar'
+
+" install kite using vimplug
+"Plug 'kiteco/vim-plugin' , { 'as': 'kite' }
+
+" Add maktaba and codefmt to the runtimepath.
+" (The latter must be installed before it can be used.)
+Plug 'google/vim-maktaba'
+Plug 'google/vim-codefmt'
+" Also add Glaive, which is used to configure codefmt's maktaba flags. See
+" `:help :Glaive` for usage.
+Plug 'google/vim-glaive'
+
+" ranger
+Plug 'francoiscabrol/ranger.vim'
+
+" pdf viewer
+Plug 'makerj/vim-pdf'
+
+" git gutter
+Plug 'airblade/vim-gitgutter'
+
+call plug#end()
